69c7f1ccbd7071157919c590692969d37ec94dd0
k2hoon <tayesymbol@gmail.com>
2023-07-08 07:00:42 +0900

    Initial Commit

---
+++ Swift-Photo/Swift_PhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_PhotoApp.swift
+//  Swift-Photo
+//
+//  Created by k2hoon on 2023/07/08.
+//
+
+import SwiftUI
+
+@main
+struct Swift_PhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
